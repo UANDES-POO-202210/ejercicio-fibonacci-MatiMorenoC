7dfbe241c96681e26298257059d226ebc6913c99
Matías Moreno <mimoreno@miuandes.cl>
2022-05-09 23:40:08 -0400

    commit

---
+++ main.js
@@ -0,0 +1,28 @@
+// metodo extraido de https://nodejs.dev/learn/reading-files-with-nodejs para leer archivos
+const fs = require('fs');
+
+fs.readFile('archivo_a_leer.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(F(data));
+});
+
+function F(n){
+
+    if (n == 0){
+
+        return 0;
+    }
+
+    else if (n == 1){
+
+        return 1;
+    }
+
+    else{
+
+        return F(n-1) + F(n-2)
+    }
+}
